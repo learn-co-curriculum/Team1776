2ac66b7c6e7f9ca88f891700101c3ae1f441b789
nrizk6 <nrizk6@gmail.com>
2015-03-30 16:35:43 -0400

    init

---
+++ union-1776-capstone/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  union-1776-capstone
+//
+//  Created by Nicolas Rizk on 3/30/15.
+//  Copyright (c) 2015 Flatiron School. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
